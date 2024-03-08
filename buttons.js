8bed22ac6e7eecaa7bd2f591709913b79d24221c
muzgog <98909464+muzgog@users.noreply.github.com>
2024-03-08 10:32:57 -0500

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function menuButton()
+{
+	if(document.getElementById('links').style.display == "none")
+	{
+		document.getElementById('links').style.display='block';
+	}
+	else if(document.getElementById('links').style.display == "block")
+	{
+		document.getElementById('links').style.display='none';
+	}
+}
+
